bd4fd08b1d4b1c8e70f095d90b4af2d96466cca0
Gondesi Mohan Geetika Sangeetha <gondesimohangeetikasangeetha@gmail.com>
2026-05-09 23:50:43 +0530

    fix time zone issue globally

--- src/main/java/com/example/demo/QuizApplication.java
+++ src/main/java/com/example/demo/QuizApplication.java
@@ -3,9 +3,18 @@ package com.example.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import jakarta.annotation.PostConstruct;
+import java.util.TimeZone;
+
 @SpringBootApplication
 public class QuizApplication {
+
     public static void main(String[] args) {
         SpringApplication.run(QuizApplication.class, args);
+    }
+
+    @PostConstruct
+    public void init() {
+        TimeZone.setDefault(TimeZone.getTimeZone("Asia/Kolkata"));
     }
 }
